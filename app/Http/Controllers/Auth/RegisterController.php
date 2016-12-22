f491355a20796b5b5733707cebc6c5042698f757
Javier Pena <javier@javierdev.com>
2016-12-21 22:20:15 -0600

    Change authentication redirects

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/snippets';
 
     /**
      * Create a new controller instance.

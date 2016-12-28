19d5fa17573d9d93011f0ca02ede2c5fea0f0313
Javier Pena <javier@javierdev.com>
2016-12-28 09:54:14 -0600

    Add syntax to index

--- resources/views/snippets/index.blade.php
+++ resources/views/snippets/index.blade.php
@@ -25,6 +25,8 @@
       <h4>{{$snippet->title}}</h4>
       <h5>Added on {{$snippet->created_at}}</h5>
       <p>{{$snippet->description}}</p>
+      <small>{{$snippet->syntax}}</small>
+      <br>
       <a href="/snippets/{{$snippet->slug}}">View</a>
     </div>
     <br>

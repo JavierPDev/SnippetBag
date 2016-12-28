1723f5f8b62ad6cd05a24c5a82a82d018ce08d83
Javier Pena <javier@javierdev.com>
2016-12-28 09:40:58 -0600

    Add snippet policy

---
+++ app/Policies/SnippetPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Snippet;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SnippetPolicy
+{
+    use HandlesAuthorization;
+
+		public function before($user, $ability)
+		{
+				if ($user->is_admin) {
+						return true;
+				}
+		}
+
+    /**
+     * Determine whether the user can view the snippet.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Snippet  $snippet
+     * @return mixed
+     */
+    public function view(User $user, Snippet $snippet)
+    {
+      return $snippet->public || $user->id == $snippet->user_id;
+    }
+
+    /**
+     * Determine whether the user can create snippets.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the snippet.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Snippet  $snippet
+     * @return mixed
+     */
+    public function update(User $user, Snippet $snippet)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the snippet.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Snippet  $snippet
+     * @return mixed
+     */
+    public function delete(User $user, Snippet $snippet)
+    {
+        //
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use App\Snippet;
+use App\Policies\SnippetPolicy;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        Snippet::class => SnippetPolicy::class,
     ];
 
     /**
